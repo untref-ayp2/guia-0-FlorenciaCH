83c550895cab4fbdb4281aaaf501bb0cc04123c3
Florencia <florenciagianna@gmail.com>
2023-03-23 23:10:21 -0300

    Update main.go

--- Funciones/main.go
+++ Funciones/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import {
+	"strconv"
+	"fmt"
+}
 
 /* 1. Definir una función que, dado los coeficientes de un polinomio de grado n (números flotantes)
  muestre por pantalla el polinomio completo,
@@ -16,17 +19,14 @@ al elegir una de ellas mostrar un cartel diciendo qué opción se eligió o si f
 
 func ImprimirPolinomio(coeficientes ...float32) {
 
-	arreglo := make([]string, len(coeficientes)) // make() crea un nuevo slice de strings
-	// con la cantidad de elementos igual a la cantidad de variables ingresadas
-
 	for i := 0; i < len(coeficientes); i++ {
 
 		if i > 1 {
-			arreglo[i] = fmt.Sprintf(" %+.1f X**%v ", coeficientes[i], i) // (%+ .1f) Con este ejemplo el input sería "-3.1416" y el output sería "- 3.1 ""
+			coeficientes[i] = fmt.Sprintf(" %+.1f X**%v ", int(coeficientes[i]), i) // (%+ .1f) Con este ejemplo el input sería "-3.1416" y el output sería "- 3.1 ""
 		} else if i == 1 {
-			arreglo[i] = fmt.Sprintf(" %+.1f X ", coeficientes[i])
+			coeficientes[i] = fmt.Sprintf(" %+.1f X ", int(coeficientes[i]))
 		} else {
-			arreglo[i] = fmt.Sprintf(" %.1f ", coeficientes[i])
+			coeficientes[i] = fmt.Sprintf(" %.1f ", int(coeficientes[i]))
 		}
 	}
 
